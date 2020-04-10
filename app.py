c72ac761ab7285ce6ef8d233c77d5c7bf94c22be
Anushka Singh <anushka318singh@gmail.com>
2020-04-10 17:53:55 +0530

    flask-jwt-extended implemented

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -19,7 +19,8 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity)  # /auth
+# this replaces authentication and identity
+jwt = JWTManager(app)  
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -27,6 +28,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db

cc0f123206b392e5c77f46f8e87ed7a398bde9d4
Anushka Singh <anushka318singh@gmail.com>
2020-04-11 14:15:24 +0530

    only fresh token for adding items

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims, get_jwt_identity
+from flask_jwt_extended import jwt_required, fresh_jwt_required, get_jwt_claims, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -23,6 +23,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'Item not found'}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
